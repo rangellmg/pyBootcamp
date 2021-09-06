31899f2facc80b3e0846c553e1b56b842d848e16
rangellmg <rangelllmg@gmail.com>
2021-09-05 22:01:44 -0300

    added new methods and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pyBootcamp
 Bootcamp Python - IGTI
+
+How to run flask:
+flask run
+
+running in debug mode:
+FLASK_ENV=development flask run
--- app.py
+++ app.py
@@ -1,8 +1,29 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # Mostrar o perfil do usuário pelo seu nome
+    return f'User Name, {escape(username)}'
+
+
+@app.route('/user/config/<uuid:id>')
+def show_user_config(id):
+    # Mostrara página de config do usuário pelo ID
+    # Ex: 123e4567-e89n-12d3-a456-426614174000
+    return f'User Identification: {id}'
+
+
+@app.route('/post/<int:post_id>')
+def show_post(username):
+    # Mostrar a postagem com o id fornecido, id é um inteiro
+    return f'Post {id}'
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # Mostrar o subcaminho após /path/
+    return f'Subpath {escape(subpath)}'
